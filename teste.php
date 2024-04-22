b2750a36057c9e2125037a9879d05d5f8c55fa1b
filipi <fvianna@gmail.com>
2024-04-22 09:06:52 -0300

    starting some tests to integrate with old ONDE fLame Work.

---
+++ teste.php
@@ -0,0 +1,13 @@
+<HTML>
+<BODY>
+<PRE>
+<?PHP
+var_dump($_POST);
+?>
+</PRE>
+<FORM ACTION="teste.php" METHOD="POST" NAME="form">
+<INPUT TYPE="TEXT" NAME="texto">
+<INPUT TYPE="SUBMIT">
+</FORM>
+</BODY>
+</HTML>
